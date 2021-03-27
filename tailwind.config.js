09681f0440d39d2b6408cd72f33ba1295b44144c
robin-whg <mail@robin-whg.com>
2021-03-27 21:34:05 +0100

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Source Code Pro", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
