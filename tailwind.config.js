f5a3fda8d9f2566bdc66a75314ab1c95806799a3
robin-whg <mail@robin-whg.com>
2021-04-15 21:36:27 +0200

    dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./public/index.html'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
       sans: ["Source Code Pro", "sans-serif"],
